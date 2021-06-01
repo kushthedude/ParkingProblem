55c30d2970e2a5fcc8b31bd2ae13d8ec175f52ae
kushthedude <kushthedude@gmail.com>
2021-06-01 16:45:32 +0530

    enh: define commands constants
    Signed-off-by: kushthedude <kushthedude@gmail.com>

---
+++ src/com/gojek/parking/Constants.java
@@ -0,0 +1,10 @@
+package com.gojek.parking;
+
+public class Constants {
+    public static final String CREATE = "create";
+    public static final String PARK = "park";
+    public static final String LEAVE = "leave";
+    public static final String STATUS = "status";
+    public static final String REG_NO_COLOR = "registration_numbers_for_cars_with_colour";
+    public static final String SLOT_NO_CAR = "slot_number_for_registration_number";
+}
