39658ddd99d239661516d826157b42469102cf9e
kushthedude <kushthedude@gmail.com>
2021-06-01 16:43:34 +0530

    feat: initialise Car class
    Signed-off-by: kushthedude <kushthedude@gmail.com>

---
+++ src/com/gojek/parking/Car.java
@@ -0,0 +1,41 @@
+package com.gojek.parking;
+
+public class Car {
+    private String registration_no;
+    private String color;
+    private int slot_no;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Car(String registration_no, String color) {
+       this(registration_no, color, 0);
+    }
+
+    public Car(String registration_no, String color, int slot_no) {
+        this.registration_no = registration_no;
+        this.color = color;
+        this.slot_no = slot_no;
+    }
+
+    public String getRegistration_no() {
+        return registration_no;
+    }
+
+    public void setRegistration_no(String registration_no) {
+        this.registration_no = registration_no;
+    }
+
+    public int getSlot_no() {
+        return slot_no;
+    }
+
+    public void setSlot_no(int slot_no) {
+        this.slot_no = slot_no;
+    }
+}
