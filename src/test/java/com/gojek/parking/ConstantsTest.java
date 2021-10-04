a63851fc0ab2d6d20ceae60e9ba634a919d9fac3
Gravish Purohit <gravish084@gmail.com>
2021-10-05 00:53:41 +0530

    changed Junit version

--- src/test/java/com/gojek/parking/ConstantsTest.java
+++ src/test/java/com/gojek/parking/ConstantsTest.java
@@ -1,16 +1,16 @@
 package com.gojek.parking;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 public class ConstantsTest{
     @Test
     public void constantsValues_shouldBeEqual(){
-        assertEquals("status", Constants.STATUS);
-        assertEquals("leave", Constants.LEAVE);
-        assertEquals("create", Constants.CREATE);
-        assertEquals("registration_numbers_for_cars_with_colour", Constants.REG_NO_COLOR);
-        assertEquals("slot_number_for_registration_number", Constants.SLOT_NO_CAR);
+        Assertions.assertEquals("status", Constants.STATUS);
+        Assertions.assertEquals("leave", Constants.LEAVE);
+        Assertions.assertEquals("create", Constants.CREATE);
+        Assertions.assertEquals("registration_numbers_for_cars_with_colour", Constants.REG_NO_COLOR);
+        Assertions.assertEquals("slot_number_for_registration_number", Constants.SLOT_NO_CAR);
     }
 }
