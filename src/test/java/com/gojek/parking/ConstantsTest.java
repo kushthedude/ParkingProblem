810c150262fe5032545215bfe1288985b48975b3
Kush Trivedi <44091822+kushthedude@users.noreply.github.com>
2021-10-05 00:55:27 +0530

    Revert "updated to Junit5 (#5)"
    This reverts commit b24ea31b414f7f168dec76d8f070de9efb431140.

--- src/test/java/com/gojek/parking/ConstantsTest.java
+++ src/test/java/com/gojek/parking/ConstantsTest.java
@@ -1,16 +1,16 @@
 package com.gojek.parking;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 
 public class ConstantsTest{
     @Test
     public void constantsValues_shouldBeEqual(){
-        Assertions.assertEquals("status", Constants.STATUS);
-        Assertions.assertEquals("leave", Constants.LEAVE);
-        Assertions.assertEquals("create", Constants.CREATE);
-        Assertions.assertEquals("registration_numbers_for_cars_with_colour", Constants.REG_NO_COLOR);
-        Assertions.assertEquals("slot_number_for_registration_number", Constants.SLOT_NO_CAR);
+        assertEquals("status", Constants.STATUS);
+        assertEquals("leave", Constants.LEAVE);
+        assertEquals("create", Constants.CREATE);
+        assertEquals("registration_numbers_for_cars_with_colour", Constants.REG_NO_COLOR);
+        assertEquals("slot_number_for_registration_number", Constants.SLOT_NO_CAR);
     }
 }
